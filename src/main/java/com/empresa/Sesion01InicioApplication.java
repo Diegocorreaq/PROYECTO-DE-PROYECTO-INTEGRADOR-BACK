b6d233ae45ca05742a2554f6ce8d88d6e1e79990
Diegocorreaq <diego.corrq@gmail.com>
2022-04-18 18:55:17 -0500

    proyecto completo

---
+++ src/main/java/com/empresa/Sesion01InicioApplication.java
@@ -0,0 +1,13 @@
+package com.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sesion01InicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sesion01InicioApplication.class, args);
+	}
+
+}
