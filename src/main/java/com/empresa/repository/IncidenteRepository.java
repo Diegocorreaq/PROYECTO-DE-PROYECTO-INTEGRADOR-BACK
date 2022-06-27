8627dd0eda1b75a7621d412a8749af500f08fd89
renatoavilam <renato2952000@hotmail.com>
2022-06-27 13:20:43 -0500

    repository

--- src/main/java/com/empresa/repository/IncidenteRepository.java
+++ src/main/java/com/empresa/repository/IncidenteRepository.java
@@ -7,16 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.empresa.entity.Incidente;
 
-
-
 public interface IncidenteRepository extends JpaRepository<Incidente, Integer>{
 	
 	@Query("select i from Incidente i where (?1 = -1 or i.departamento.codDepartamento = ?1) and (?2 = -1 or i.causaIncidente.idCausaIncidente = ?2)  and (i.estado = ?3) ")
 	public List<Incidente> listaIncidentePorCausaIncidenteEstado(int coddepartamento, int idCausaIncidente, int estado); 
-	
-	
-
-	
 	
 	
 
